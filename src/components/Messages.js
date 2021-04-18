f2563612abad37de9fc3d1a09f669e3e478f5445
Stefan Hristov <shristov22@gmail.com>
2021-04-17 17:50:03 -0700

    added auto-scrolling to chat

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,34 +1,55 @@
 import { useSelector } from "react-redux";
-import {MESSAGE_TYPE} from '../redux/stateConstans'
+import { MESSAGE_TYPE } from "../redux/stateConstans";
 import { getMessages } from "../redux/messages";
+import ReactScrollableFeed from "react-scrollable-feed";
 
-const generateMessage = ({username, text, type}) => {
+const generateMessage = ({ username, text, type }) => {
   switch (type) {
     case MESSAGE_TYPE.JOIN:
-      return <td className="text-secondary"><strong>{text}</strong></td>;
+      return (
+        <td className="text-secondary">
+          <strong>{text}</strong>
+        </td>
+      );
     case MESSAGE_TYPE.ANSWER:
     case MESSAGE_TYPE.CORRECT:
-      return <td className="text-success"><strong>{text}</strong></td>;
+      return (
+        <td className="text-success">
+          <strong>{text}</strong>
+        </td>
+      );
     case MESSAGE_TYPE.GAME_OVER:
     case MESSAGE_TYPE.LEAVE:
-      return <td className="text-danger"><strong>{text}</strong></td>;
-    default: // MESSAGE_TYPE.REGULAR
-      return <td><strong>{username}:</strong>{text}</td>;
+      return (
+        <td className="text-danger">
+          <strong>{text}</strong>
+        </td>
+      );
+    default:
+      // MESSAGE_TYPE.REGULAR
+      return (
+        <td>
+          <strong>{username}: </strong>
+          {text}
+        </td>
+      );
   }
-}
+};
 
 const Messages = () => {
   const messages = useSelector(getMessages);
   return (
-    <table className="table table-striped table-bordered my-4">
-      <tbody>
-        {messages.map((message, index) => (
-          <tr key={`msg-${index}`}>
-            {generateMessage(message)}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className="messages" style={{ overflowY: "scroll", height: "300px" }}>
+      <ReactScrollableFeed forceScroll>
+        <table className="table table-striped table-bordered">
+          <tbody>
+            {messages.map((message, index) => (
+              <tr key={`msg-${index}`}>{generateMessage(message)}</tr>
+            ))}
+          </tbody>
+        </table>
+      </ReactScrollableFeed>
+    </div>
   );
 };
 
