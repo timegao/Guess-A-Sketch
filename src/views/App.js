65d5c049af5ac8d6f39c9443c22e9e0c56fbe7e8
Stefan Hristov <shristov22@gmail.com>
2021-04-18 14:45:04 -0700

    changed view depending on number of players

--- src/views/App.js
+++ src/views/App.js
@@ -5,11 +5,29 @@ import { getPlayer } from "../redux/player";
 import GameView from "../components/GameView";
 import CanvasInputs from "../components/CanvasInputs";
 import { INITIAL_STROKE } from "../redux/stateConstants";
+import { getUsers } from "../redux/users";
+import Loading from "../components/Loading";
 
 const App = () => {
+  const MIN_PLAYERS = 2;
   const player = useSelector(getPlayer);
+  const users = useSelector(getUsers);
 
-  return <>{player.hasOwnProperty("username") ? <GameView /> : <JoinChat />}</>;
+  // return <>{player.hasOwnProperty("username") ? <GameView /> : <JoinChat />}</>;
+
+  if (!player.hasOwnProperty("username")) {
+    return <JoinChat />;
+  } else if (
+    player.hasOwnProperty("username") &&
+    Object.keys(users).length < MIN_PLAYERS
+  ) {
+    return <Loading msg={"Waiting for another player to Join..."} />;
+  } else if (
+    player.hasOwnProperty("username") &&
+    Object.keys(users).length >= MIN_PLAYERS
+  ) {
+    return <GameView />;
+  }
 };
 
 export default App;
