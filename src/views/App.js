3be58fe24be3a2e7b02787b2c6c03e1e355fdc63
timegao <50337470+timegao@users.noreply.github.com>
2021-04-20 00:11:12 -0700

    adds new modal placeholders for testing dynamic rendering

--- src/views/App.js
+++ src/views/App.js
@@ -2,22 +2,11 @@ import { useSelector } from "react-redux";
 import JoinChat from "../components/JoinChat";
 import { getPlayer } from "../redux/player";
 import GameView from "../components/GameView";
-import Loading from "../components/Loading";
-import { getGameState } from "../redux/game";
-import { GAME_STATE } from "../redux/stateConstants";
 
 const App = () => {
   const player = useSelector(getPlayer);
-  const gameState = useSelector(getGameState);
 
-  if (player.hasOwnProperty("username")) {
-    if (gameState === GAME_STATE.GAME_WAITING) {
-      return <Loading msg={"Waiting for another player to Join..."} />;
-    } else {
-      return <GameView />;
-    }
-  }
-  return <JoinChat />;
+  return <>{player.hasOwnProperty("username") ? <GameView /> : <JoinChat />}</>;
 };
 
 export default App;
