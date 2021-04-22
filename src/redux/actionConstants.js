501c50720555dc86abb198d7f1b15a20249f5a73
Stefan Hristov <shristov22@gmail.com>
2021-04-21 18:00:10 -0700

    module export for words.js and added SET_WORD_CHOICES action constant

--- src/redux/actionConstants.js
+++ src/redux/actionConstants.js
@@ -10,3 +10,4 @@ export const SET_TURN_END = "game/setTurnEnd";
 export const SET_GAME_OVER = "game/setGameOver";
 export const COUNTDOWN_TIMER = "game/countdownTimer";
 export const UPDATE_GAME = "game/updateGame";
+export const SET_WORD_CHOICES = "game/setWordChoices";
