6ea9302965d70430053c2b9fc32258cb1847682d
Stefan Hristov <shristov22@gmail.com>
2021-04-21 17:54:49 -0700

    added wordToGuess and wordChoices to game object and created listeners for choosing word

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 /** SERVER CONFIGURATION */
 const express = require("express");
+const { getWordChoicesData } = require("./src/data/words");
 const {
   INITIAL_GAME,
   MESSAGE_TYPE,
@@ -28,9 +29,9 @@ const PORT = process.env.PORT || 4002;
 app.use(express.static(__dirname + "/"));
 
 let lines = []; // Array of lines drawn on Canvas
-let wordToGuess = "correct"; // Word for users to guess
+// let wordToGuess = "correct"; // Word for users to guess
 let hint = ""; // Hint for guessers to see
-let game = INITIAL_GAME; // Stores gameState, timer, and round
+let game = INITIAL_GAME; // Stores gameState, timer, round, wordChoices, and wordToGuess
 let MAX_DIFF_CLOSE_GUESS = 2; // characters difference to consider a close guess
 
 const clients = {}; // Object to map client ids to their usernames
@@ -203,12 +204,12 @@ const updateMessageText = (username, msgText, type) => {
  */
 const guessRelativeDifference = (msgText) => {
   const guessSize = msgText.length;
-  const answerSize = wordToGuess.length;
+  const answerSize = game.wordToGuess.length;
 
   let i = 0;
   let differenceCount = 0;
   while (i < guessSize && i < answerSize) {
-    if (msgText.charAt(i).toLowerCase() !== wordToGuess.charAt(i)) {
+    if (msgText.charAt(i).toLowerCase() !== game.wordToGuess.charAt(i)) {
       differenceCount++;
     }
     i++;
@@ -274,6 +275,18 @@ io.on("connection", (client) => {
 
   client.on("new word", (word) => {
     game.wordToGuess = word;
-    game.timer = 0; // Skips rest of GAME_STATE.TURN_START
+    game.timer = 0;
+    client.emit("update game", game); // set word for current drawing client
+    client.broadcast.emit("update game", {
+      ...game,
+      wordChoices: {},
+      wordToGuess: "",
+    }); // all other users just reset timer
+    countdownTurnStart(); // force transition to turn during because time is <=0 and game state equals turn during
+  });
+
+  client.on("get words to choose from", () => {
+    game.wordChoices = getWordChoicesData();
+    client.emit("choose word", game.wordChoices);
   });
 });
