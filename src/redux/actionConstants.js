f6636dc3781ce27edd6d7a092c89cc62e0a512c4
Stefan Hristov <shristov22@gmail.com>
2021-04-23 22:38:53 -0700

    added setHint constant to action constants

--- src/redux/actionConstants.js
+++ src/redux/actionConstants.js
@@ -1,6 +1,7 @@
 export const UPDATE_MESSAGES = "messages/updateMessages";
 export const ADD_LINE = "lines/addLine";
 export const ADD_PLAYER = "player/addPlayer";
+export const INVALID_USERNAME = "player/setInvalidUsername";
 export const UPDATE_USERS = "users/updateUsers";
 export const UPDATE_USER = "users/updateUser";
 export const SET_GAME_WAITING = "game/setGameWaiting";
@@ -12,4 +13,4 @@ export const COUNTDOWN_TIMER = "game/countdownTimer";
 export const UPDATE_GAME = "game/updateGame";
 export const SET_WORD_CHOICES = "game/setWordChoices";
 export const SET_WORD_TO_GUESS = "game/setWordToGuess";
-export const INVALID_USERNAME = "player/setInvalidUsername";
+export const SET_HINT = "game/setHint";
