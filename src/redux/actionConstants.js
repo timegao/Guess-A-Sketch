775b7e77b37bc45dea93b69e5d52099f75fae6ba
Stefan Hristov <shristov22@gmail.com>
2021-04-25 14:43:44 -0700

    added LOGOUT action Constant

--- src/redux/actionConstants.js
+++ src/redux/actionConstants.js
@@ -2,6 +2,7 @@ export const UPDATE_MESSAGES = "messages/updateMessages";
 export const ADD_LINE = "lines/addLine";
 export const ADD_PLAYER = "player/addPlayer";
 export const INVALID_USERNAME = "player/setInvalidUsername";
+export const LOGOUT = "player/logout";
 export const UPDATE_USERS = "users/updateUsers";
 export const UPDATE_USER = "users/updateUser";
 export const SET_GAME_WAITING = "game/setGameWaiting";
