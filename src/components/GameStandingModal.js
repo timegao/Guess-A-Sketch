917a64ef9f98c2ccce787177c7955a58083b96b4
Stefan Hristov <shristov22@gmail.com>
2021-04-27 12:04:55 -0700

    styling of standings when game over

--- src/components/GameStandingModal.js
+++ src/components/GameStandingModal.js
@@ -1,5 +1,7 @@
 import { useSelector } from "react-redux";
 import { getUsers } from "../redux/users";
+import { AVATAR_MAP } from "../redux/stateConstants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 /** Helper to sort users in descending order based on score*/
 const sortUsersScore = (users) => {
@@ -16,9 +18,20 @@ const GameStandingModal = () => {
       <ul className="score-modal">
         {sortedUsers.map((user, i) => (
           <li className="player-score" key={i}>
-            <div className="row">
-              <div className="col">{i + 1 + ". " + user.username}</div>
-              <div className="col">{user.scoring.score + " points"}</div>
+            <div className="row mt-1">
+              <div className="col mt-2">
+                <span className="fs-3">{i + 1 + ". "}</span>
+                <span>
+                  <FontAwesomeIcon icon={AVATAR_MAP[user.avatar]} size="2x" />
+                </span>
+              </div>
+              <div
+                className="col mt-1"
+                style={{ overflow: "hidden", textOverflow: "ellipsis" }}
+              >
+                {user.username}
+              </div>
+              <div className="col score-earned mt-1">{user.scoring.score}</div>
             </div>
           </li>
         ))}
