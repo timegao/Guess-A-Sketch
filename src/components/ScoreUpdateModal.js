113c0261c0296fc738c6008b4ee229d6d10d9cba
Pedro Cantu <pedro@Pedros-MBP.domain>
2021-04-26 21:41:29 -0700

    added avatar in score update modal

--- src/components/ScoreUpdateModal.js
+++ src/components/ScoreUpdateModal.js
@@ -1,6 +1,8 @@
 import { useSelector } from "react-redux";
 import { getUsers } from "../redux/users";
 import { getPickedWord } from "../redux/word";
+import { AVATAR_MAP } from "../redux/stateConstants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 /** Helper to sort users in descending order based on earned*/
 const sortUsersWonTurn = (users) => {
@@ -21,6 +23,11 @@ const ScoreUpdateModal = () => {
         {sortedUsers.map((user, i) => (
           <li className="player-score" key={i}>
             <div className="row">
+              <div className="col">
+                <span className="float-start">
+                  <FontAwesomeIcon icon={AVATAR_MAP[user.avatar]} size="2x" />
+                </span>
+              </div>
               <div className="col">{user.username}</div>
               <div className="col score-earned">+{user.scoring.earned}</div>
             </div>
