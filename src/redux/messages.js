6d9ae6ae998c622cd312c159b9b1935166016b3d
Pedro Cantu <pedro@Pedros-MBP.domain>
2021-04-29 13:01:54 -0700

    clear messages at logout

--- src/redux/messages.js
+++ src/redux/messages.js
@@ -1,10 +1,12 @@
 import { INITIAL_MESSAGES } from "./stateConstants";
-import { UPDATE_MESSAGES } from "./actionConstants";
+import { UPDATE_MESSAGES, LOGOUT } from "./actionConstants";
 
 const messagesReducer = (state = INITIAL_MESSAGES, action) => {
   switch (action.type) {
     case UPDATE_MESSAGES:
       return [...state, action.payload.message];
+    case LOGOUT:
+      return [...INITIAL_MESSAGES];
     default:
       return state;
   }
